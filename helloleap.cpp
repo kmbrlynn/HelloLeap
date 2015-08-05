e1e7b11bd05a75e0a31f634460dab8295c360f5f
kmbrlynn <douglas.kimberly.a@gmail.com>
2015-08-04 21:09:40 -0400

    implement custom listener class

--- helloleap.cpp
+++ helloleap.cpp
@@ -1,1 +1,10 @@
+#include "Leap.h"
 #include "helloleap.hpp"
+
+void KimsListener::onConnect(const Controller& controller) {
+    std::cout << "Connected" << std::endl;
+}
+
+void KimsListener::onFrame(const Controller & controller) {
+    std::cout << "Frame available" << std::endl;
+}
