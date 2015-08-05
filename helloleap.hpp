cabf0d4b3200994023442048ca4411a35f451e7b
kmbrlynn <douglas.kimberly.a@gmail.com>
2015-08-04 21:09:21 -0400

    header guards, declare custom listener class

--- helloleap.hpp
+++ helloleap.hpp
@@ -1,1 +1,13 @@
-// header stuff
+#ifndef HELLOLEAP_H
+#define HELLOLEAP_H
+#include "Leap.h"
+
+using namespace Leap;
+
+class KimsListener : public Listener {
+    public:
+        virtual void onConnect(const Controller&);
+        virtual void onFrame(const Controller&);
+};
+
+#endif
