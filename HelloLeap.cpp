79c9ca339233522a7ede06fb093f8bf8a75d044f
kmbrlynn <douglas.kimberly.a@gmail.com>
2015-08-04 22:33:58 -0400

    add Listener

--- HelloLeap.cpp
+++ HelloLeap.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string.h>
+#include <string>
 #include "helloleap.hpp"
 #include "Leap.h"
 
@@ -7,9 +7,24 @@ using namespace Leap;
 
 int main(int argc, char* argv[]) {
 
-    // keep this process running until user hits Enter
+    // Connects, asynchronously, to the Leap Motion service.
+    // If there is a successful connection && Leap is plugged in, 
+    // then controller.isConnected() = true;
+    // This invokes the Listener.onConnect() callback function
+    Controller controller;
+    
+    // The asynchronous Controller can be polled for frames (or other state data), 
+    // or it can use a Listener. We will use a Listener for this program
+    KimsListener listener;
+
+    controller.addListener(listener);
+
+    // Keep this process running until user hits Enter
     std::cout << "Hit Enter to quit..." << std::endl;
     std::cin.get();
+
+    // Remove the listener when you're done
+    controller.removeListener(listener);
 
     return 0;
 }
